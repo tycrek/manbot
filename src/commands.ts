c3dad82e5abc632923f4aae610b98bbc2d43358c
Josh Moore <josh.moore+gpg@jmoore.dev>
2023-09-24 20:00:29 -0600

    feat: added commands

---
+++ src/commands.ts
@@ -0,0 +1,26 @@
+import { Command } from './interactions';
+import { ApplicationCommandOptionType } from './enums';
+
+const TokeCommand: Command = {
+	name: 'toke',
+	description: 'Time for a toke',
+	run: (interaction) => Promise.resolve(`It's 4:20 somewhere, toke up!`)
+};
+
+const CheersCommand: Command = {
+	name: 'cheers',
+	description: 'Time for a drink',
+	run: (interaction) => Promise.resolve(`Rounds on me, bottoms up!`)
+};
+
+const AussieCommand: Command = {
+	name: 'aussie',
+	description: 'OI OI OI',
+	run: (interaction) => Promise.resolve(`aussie aussie aussie`)
+};
+
+export const Commands = [
+	TokeCommand,
+	CheersCommand,
+	AussieCommand,
+];
